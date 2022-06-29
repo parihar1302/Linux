b409f828b1553f437a1bc077c70cce7430fc854c
parihar1302 <45783975+parihar1302@users.noreply.github.com>
2022-06-29 21:18:16 +0530

    coloringtext

---
+++ colortext.sh
@@ -0,0 +1,12 @@
+## Showing  how you can color text in your file
+
+#!/bin/bash
+echo "this is Abhishek Singh Parihar"
+echo in the house
+
+
+## now showing the output in color text
+
+echo -e "\033[0;31m failed message"
+echo -e "\033[0;32m successfull message"
+echo -e "\033[0;33m warning message"
